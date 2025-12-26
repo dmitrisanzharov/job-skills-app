055b22da1f82db1e23359fc3442d8ccf9e58258f
fruitloaf <dmitrisanzharov@gmail.com>
2025-12-26 12:13:18 +0000

    all good

--- src/consts/globalVars.ts
+++ src/consts/globalVars.ts
@@ -1,2 +1,5 @@
 export const skillMinPercentageToFilter: number = 0.1;
-export const skillThatIWillNotTake: string[] = ['angular', 'vue', 'playwright', 'vite', 'Job Interview'];
+
+// remove skills that I will not take into account
+// cloud platforms = AWS covers this so remove
+export const skillThatIWillNotTake: string[] = ['angular', 'vue', 'playwright', 'vite', 'Job Interview', 'cloud platforms'];
