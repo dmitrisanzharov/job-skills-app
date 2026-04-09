6f4aa2776c09b31541b889a442c5203fd9d10778
VictorSarov <victor.sarov@rentalmatics.com>
2026-04-09 15:48:11 +0100

    good stuff

--- src/consts/globalVars.ts
+++ src/consts/globalVars.ts
@@ -2,4 +2,22 @@ export const skillMinPercentageToFilter: number = 0.1;
 
 // remove skills that I will not take into account
 // cloud platforms = AWS covers this so remove
-export const skillThatIWillNotTake: string[] = ['Job Interview', 'angular', 'vue', 'playwright', 'vite', 'cloud platforms'];
+
+
+const skillsThatIRemovedWithExplainer: { skillName: string; reason: string }[] = [
+    // temporary disabled
+    { skillName: 'Job Interview', reason: 'I will not take into account skills related to job interviews' },
+    { skillName: 'ui/ux', reason: 'temporary disabled as of 9-Apr-2026' },
+    { skillName: 'accessibility', reason: 'temporary disabled as of 9-Apr-2026' },
+    { skillName: 'Security', reason: 'temporary disabled as of 9-Apr-2026' },
+
+    // not taking at all
+    { skillName: 'angular', reason: 'React focus' },
+    { skillName: 'vue', reason: 'React focus' },
+    { skillName: 'playwright', reason: 'Cypress covers this' },
+    { skillName: 'vite', reason: 'Webpack focus' },
+    { skillName: 'cloud platforms', reason: 'AWS covers this already' },
+];
+
+
+export const skillThatIWillNotTake: string[] = skillsThatIRemovedWithExplainer.map(skill => skill.skillName);
